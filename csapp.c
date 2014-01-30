6e9f728868f7e7dee675050b3044b35f94c74a9c
Zachary Michaelov <zach.michaelov@gmail.com>
2014-01-29 22:42:08 -0500

    fixed typo

--- csapp.c
+++ csapp.c
@@ -808,7 +808,7 @@ void Rio_writep(int fd, void *usrbuf, size_t n)
 {
     if (rio_writep(fd, usrbuf, n) != n) {
         if (errno != EPIPE)
-	   unix_error("Rio_writen error");
+	   unix_error("Rio_writep error");
 	else {
 //	   printf("Rio_writen: socket has closed with EPIPE\n");
         }
